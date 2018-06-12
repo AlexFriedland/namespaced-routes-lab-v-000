c11b5002ecf97cf58494a40fad587962e89ad690
AlexFriedland <friedlandaj@gmail.com>
2018-06-12 11:23:13 +0200

    cp3

--- app/controllers/admin/preferences_controller.rb
+++ app/controllers/admin/preferences_controller.rb
@@ -1,4 +1,14 @@
 class Admin::PreferencesController < ApplicationController
   def index
   end
+
+  def update
+  end
+
+  private
+
+  def preference_params
+    params.require(:preference).permit(:allow_create_songs, :allow_create_artists, :song_sort_order, :artist_sort_order)
+  end
+
 end
